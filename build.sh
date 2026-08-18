2b2889de326ee8f18e30c3dbfad0af62b64813e1
Sergey Kovalev <info@neolite.ru>
2026-08-18 18:42:50 +0300

    Fix development build script and include CodeIntel resources

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 set -eu
 
 ROOT=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-OUT=${1:-"$ROOT/../komodo-typescript-9.3.2-0.1.0.xpi"}
+VERSION=$(sed -n 's:.*<em:version>\([^<]*\)</em:version>.*:\1:p' "$ROOT/install.rdf" | head -n 1)
+OUT=${1:-"$ROOT/../komodo-typescript-9.3.2-${VERSION}.xpi"}
 
 rm -f "$OUT"
 cd "$ROOT"
-zip -9 -r "$OUT" install.rdf components support LICENSE README.md \
-    -x '*/__pycache__/*' '*.pyc' '*.pyo'
+zip -9 -r "$OUT" \
+    install.rdf chrome.manifest components content pylib support skin test \
+    LICENSE README.md docs \
+    -x '*/__pycache__/*' '*.pyc' '*.pyo' '.git/*'
 
 echo "$OUT"
